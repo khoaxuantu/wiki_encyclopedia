90dbef556d98071c4fd308ee57355e981dcbe644
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-02-12 00:08:37 +0800

    Update entry pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +10,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+""" Display the entry page """
+def entry(request):
+    if request.method == "POST":
+        entry = request.POST["q"]
+        content = util.get_entry(entry)
+
+        if content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry,
+                "md": markdown2.markdown(content)
+            })
+        else:
+            return HttpResponse("404 Not Found!")
+            
+    else:
+        return index(request)
