4d95bf4da33068fca02e58ce57e7f570a2d03d73
khoaxuantu <68913255+khoaxuantu@users.noreply.github.com>
2023-02-13 00:38:23 +0800

    Add 'create page'

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.generate_entry, name="entry"),
     path("search", views.entry, name="search"),
+    path("createpage", views.create_page, name="create page")
 ]
